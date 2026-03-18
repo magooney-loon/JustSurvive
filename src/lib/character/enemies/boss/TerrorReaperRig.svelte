48be960af0235af0e2cd87b2818734d4a020ee0b
Magooney <magooney-loon@proton.me>
2026-03-18 15:29:23 +0100

    Update TerrorReaperRig.svelte

--- src/lib/character/enemies/boss/TerrorReaperRig.svelte
+++ src/lib/character/enemies/boss/TerrorReaperRig.svelte
@@ -158,7 +158,9 @@
 				: attackPhase > 0.15
 					? 'attack'
 					: speed > 0.1
-						? (speed > 6 ? 'run' : 'walk')
+						? speed > 6
+							? 'run'
+							: 'walk'
 						: 'idle';
 
 		const current = $actions[currentAction];
@@ -182,35 +184,43 @@
 	refDistance={3}
 	maxDistance={20}
 	rolloffFactor={1.5}
-	oncreate={(a) => { footstepAudio = a; }}
+	oncreate={(a) => {
+		footstepAudio = a;
+	}}
 />
 <PositionalAudio
 	src="{import.meta.env.BASE_URL}sounds/enemies/boss_attack.mp3"
 	refDistance={5}
 	maxDistance={25}
 	rolloffFactor={1.5}
-	oncreate={(a) => { attackAudio = a; }}
+	oncreate={(a) => {
+		attackAudio = a;
+	}}
 />
 <PositionalAudio
 	src="{import.meta.env.BASE_URL}sounds/enemies/boss_dead.mp3"
 	refDistance={8}
 	maxDistance={30}
 	rolloffFactor={1.5}
-	oncreate={(a) => { deadAudio = a; }}
+	oncreate={(a) => {
+		deadAudio = a;
+	}}
 />
 <PositionalAudio
 	src="{import.meta.env.BASE_URL}sounds/enemies/boss_daze.mp3"
 	refDistance={5}
 	maxDistance={25}
 	rolloffFactor={1.5}
-	oncreate={(a) => { dazeAudio = a; }}
+	oncreate={(a) => {
+		dazeAudio = a;
+	}}
 />
 
 <GLTF
 	bind:gltf={$gltf}
 	url="{import.meta.env.BASE_URL}models/enemies/boss/terror_reaper/scene.gltf"
 	rotation.y={Math.PI}
-	scale={3.2}
+	scale={2.7}
 />
 
 <!-- Soul Drain VFX: converging dark purple tendrils + red ring -->
